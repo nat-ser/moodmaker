283fca8132c23d105a4249271a168a42edd85a16
nat-ser <nataliya.seryakova@gmail.com>
2016-01-03 13:10:30 -0500

    Something works

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,3 +1,4 @@
 class Mood < ActiveRecord::Base
   has_many :movies
+  validates_presence_of :name, message: "You need to talk about your feeeelings!!" 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
   belongs_to :mood
+  validates_presence_of :name,:director, :info, :effect_rating, :mood_id, message: "FILL IN ALL THE FIELDS!!"
+  validates :effect_rating, numericality: { only_integer: true, message: "Fill in a number!!" }
 end
